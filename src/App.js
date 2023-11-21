8568b2bb64af0bfe8b46184458b47d6aa41f8277
Mayank039 <114925849+Mayank039@users.noreply.github.com>
2023-11-21 10:27:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Restaurant from './component/Basic/Restaurant';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+// import UseState from './component/Hooks/useState';
+
+function App() {
+  return (
+  <>
+  {/* <Restaurant/> */}
+  {/* <UseState/> */}
+  {/* <UseEffect/> */}
+  <UseReducer/>
+  </>
+  );
+}
+
+export default App;
